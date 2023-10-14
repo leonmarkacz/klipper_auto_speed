c0d440a4c74375a27becab1a780b7afc5bb0b24b
Leon Markacz <61313960+leonmarkacz@users.noreply.github.com>
2023-10-15 01:15:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-# automatically calculate your printer's maximum acceleration/velocity
+# Automatically link auto_speed to Klipper
 #
 # Copyright (C) 2023 Anonoei <dev@anonoei.com>
 #
 # This file may be distributed under the terms of the MIT license.
 
-# Force script to exit if an error occurs
-set -e
-
+# Constants
 KLIPPER_PATH="${HOME}/klipper"
 SYSTEMDDIR="/etc/systemd/system"
-SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+SRCDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+KLIPPER_SERVICE="klipper.service"
 
-# Verify we're running as root
+# Verify we're not running as root
 if [ "$(id -u)" -eq 0 ]; then
-    echo "This script must not run as root"
-    exit -1
+    echo "Error: This script must not run as root."
+    exit 1
 fi
 
 # Check if Klipper is installed
-if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
-    echo "Klipper service found!"
-else
-    echo "Klipper service not found, please install Klipper first"
-    exit -1
+if ! sudo systemctl list-units --full -all -t service --no-legend | grep -Fq "$KLIPPER_SERVICE"; then
+    echo "Error: Klipper service not found. Please install Klipper first."
+    exit 1
 fi
 
-# Link auto speed to klipper
-echo "Linking auto speed to Klipper..."
+# Link auto_speed to Klipper
+echo "Linking auto_speed to Klipper..."
 ln -sf "${SRCDIR}/auto_speed.py" "${KLIPPER_PATH}/klippy/extras/auto_speed.py"
 
-# Restart klipper
+# Restart Klipper
 echo "Restarting Klipper..."
 sudo systemctl restart klipper
